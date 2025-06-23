1ae42f6e89efe57e044cbc001781840eb3ac8a0b
Abu Hanife Nayem <ahnayem.sust@gmail.com>
2025-06-23 12:28:11 +0600

    template

--- kito-sizechart/app/services/sizecharts.crud.js
+++ kito-sizechart/app/services/sizecharts.crud.js
@@ -1,44 +1,41 @@
 import db from "../db.server";
 
-let sizeCharts = [
-  { id: "1", title: "Men's Shirts", content: "Size info here" },
-  { id: "2", title: "WoMen's Shirts", content: "Size info here" },
-  { id: "3", title: "All Shirts", content: "Size info here" },
-];
-
 export async function getCharts() {
   const allCharts = await db.sizeChart.findMany({
     orderBy: { createdAt: "desc" }
   });
-  // await db.sizeChart.create({data:{
-  //   "title": "AAA",
-  //   "content": "AAA",
-  // }});
+
   return allCharts;
 }
 
 export async function getChartById(id) {
-  return sizeCharts.find(chart => chart.id === id);
+
+  return await db.sizeChart.findUnique({
+    where: {
+      id:Number(id)
+    }
+  });
 }
 
 export async function createChart({ title, content }) {
-  // const newChart = { id: String(Date.now()), title, content };
-  // sizeCharts.push(newChart);
   
-  const newChart = await db.sizeChart.create({
-    data: {
-      title,
-      content
-    }
-  });
+  const response = await db.sizeChart.create({data:{
+    "title": title,
+    "content": content,
+  }});
 
-  return newChart;
+  return response;
 }
 
 export async function updateChart(id, { title, content }) {
-  const chart = await getChartById(id);
-  if (chart) {
-    chart.title = title;
-    chart.content = content;
-  }
+  const updateChart = await db.sizeChart.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      title: title,
+      content: content
+    },
+  });
+  return updateChart;
 }

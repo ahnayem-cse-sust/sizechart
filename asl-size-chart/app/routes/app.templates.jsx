b5072911af10d76ebf7a7de874ff812522b598da
Abu Hanife Nayem <ahnayem.sust@gmail.com>
2025-06-29 15:49:54 +0600

    temp

--- asl-size-chart/app/routes/app.templates.jsx
+++ asl-size-chart/app/routes/app.templates.jsx
@@ -1,83 +1,41 @@
-import {
-  Box,
-  Card,
-  Layout,
-  Link,
-  List,
-  Page,
-  Text,
-  BlockStack,
-} from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
+import { List, Text, Button } from '@shopify/polaris';
+import { useLoaderData, Link } from '@remix-run/react';
+import { getList } from '../services/template';
 
-export default function AdditionalPage() {
-  return (
-    <Page>
-      <TitleBar title="Additional page" />
-      <Layout>
-        <Layout.Section>
-          <Card>
-            <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                The app template comes with an additional page which
-                demonstrates how to create multiple pages within app navigation
-                using{" "}
-                <Link
-                  url="https://shopify.dev/docs/apps/tools/app-bridge"
-                  target="_blank"
-                  removeUnderline
-                >
-                  App Bridge
-                </Link>
-                .
-              </Text>
-              <Text as="p" variant="bodyMd">
-                To create your own page and have it show up in the app
-                navigation, add a page inside <Code>app/routes</Code>, and a
-                link to it in the <Code>&lt;NavMenu&gt;</Code> component found
-                in <Code>app/routes/app.jsx</Code>.
-              </Text>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section variant="oneThird">
-          <Card>
-            <BlockStack gap="200">
-              <Text as="h2" variant="headingMd">
-                Resources
-              </Text>
-              <List>
-                <List.Item>
-                  <Link
-                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-                    target="_blank"
-                    removeUnderline
-                  >
-                    App nav best practices
-                  </Link>
-                </List.Item>
-              </List>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+
+export async function loader() {
+  const templates = await getList();
+  return Response.json({ templates });
 }
 
-function Code({ children }) {
+
+export default function SizeChartTemplates() {
+  const { templates } = useLoaderData();
+
+
   return (
-    <Box
-      as="span"
-      padding="025"
-      paddingInlineStart="100"
-      paddingInlineEnd="100"
-      background="bg-surface-active"
-      borderWidth="025"
-      borderColor="border"
-      borderRadius="100"
-    >
-      <code>{children}</code>
-    </Box>
+    <div style={{ padding: 20 }}>
+      
+      <Button>
+        Create New Template
+      </Button>
+      <br/>
+
+      <Text variant="headingLg" as="h5" alignment="center">
+        All Existing Templates
+      </Text>
+      <br />
+      <List type='number'>
+        {templates.map(template => (
+          <List.Item key={template.id}>
+            <strong>{template.title}</strong>
+            &nbsp;
+            {/* <Link to={`/app/chart/${template.id}`}>Edit</Link> */}
+          </List.Item>
+        ))}
+      </List>
+
+
+    </div>
   );
 }

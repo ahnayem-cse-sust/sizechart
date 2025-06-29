a8ba82609b3e9b1a1b5e19417dfbcd86307c9cb3
Abu hanife nayem <ahnayem.sust@gmail.com>
2025-06-30 00:13:58 +0600

    temp

--- asl-size-chart/app/routes/app.templates.create.jsx
+++ asl-size-chart/app/routes/app.templates.create.jsx
@@ -1,36 +1,63 @@
+import {useState, useCallback} from 'react';
 import {
   PageActions,
   Card,
   Layout,
-  Link,
-  List,
   Page,
   Text,
   Button,
+  Form, FormLayout, Checkbox, TextField,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
-export default function TemplateCreate() {
+export default function TemplateCreateForm() {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = useCallback(() => {
+    setTitle('');
+  }, []);
+
+  const handleTitleChange = useCallback((value) => setEmail(value), []);
+
   return (
     <Page>
-      <PageActions
-      primaryAction={{
-        content: 'Save',
-      }}
-      secondaryActions={[
-        {
-          content: 'Cancel',
-        },
-      ]}
-    />
       <TitleBar title="Size Chart \ Template \ Create" />
+
+      <PageActions
+        primaryAction={{
+          content: 'Save',
+        }}
+        secondaryActions={[
+          {
+            content: 'Cancel',
+          },
+        ]}
+      />
       <Layout>
         <Layout.Section>
           <Card>
             <Button url='/app/templates'>
               Back
             </Button>
-            <p>FFFFFFF</p>
+            <Form onSubmit={handleSubmit}>
+              <FormLayout>
+
+                <TextField
+                  value={title}
+                  onChange={handleTitleChange}
+                  label="Title"
+                  type="text"
+                  autoComplete=""
+                  helpText={
+                    <span>
+                      This title will help to find desired template on chart creation.
+                    </span>
+                  }
+                />
+
+                <Button submit>Submit</Button>
+              </FormLayout>
+            </Form>
           </Card>
         </Layout.Section>
 

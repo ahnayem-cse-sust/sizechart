f751afed3b759bda5022544001a274b852748dc4
Abu hanife nayem <ahnayem.sust@gmail.com>
2025-07-02 23:59:47 +0600

    file upload

--- asl-size-chart/app/routes/app.templates.create.jsx
+++ asl-size-chart/app/routes/app.templates.create.jsx
@@ -6,7 +6,8 @@ import {
   Page,
   Grid,
   Button,
-  Form, FormLayout, ButtonGroup, TextField, Text
+  Form, FormLayout, ButtonGroup, TextField, Text,
+  DropZone
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { XIcon } from "@shopify/polaris-icons";
@@ -24,6 +25,7 @@ export default function TemplateCreateForm() {
   const [newSize, setNewSize] = useState('');
   const [errorNewSize, setErrorNewSize] = useState(null);
   const [description, setDescription] = useState(null);
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     // Dynamically load Quill client-side
@@ -67,6 +69,44 @@ export default function TemplateCreateForm() {
   const handleDescriptionChange = (value) => {
     setDescription(value);
   };
+
+  const handleDropZoneDrop = useCallback(
+    (_dropFiles, acceptedFiles, _rejectedFiles) => {
+      if (acceptedFiles.length > 0) {
+        setFile(acceptedFiles[0]); // Only accept first file
+      }
+    },
+    []
+  );
+
+  const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+
+  const fileUpload = !file && <DropZone.FileUpload actionTitle="Upload image" actionHint="Accepts .gif, .jpeg, and .png" />;
+
+  const uploadedFiles = file && (
+    <div style={{ padding: '20px'}}>
+      {validImageTypes.includes(file.type) ? (
+        <div style={{ width: '30%', height: '150px', overflow: 'hidden',margin: 'auto' }}>
+          <img
+            src={
+              validImageTypes.includes(file.type)
+                ? window.URL.createObjectURL(file)
+                : NoteIcon
+            }
+            alt="Uploaded preview"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              borderRadius: 'var(--p-border-radius-base)',
+            }}
+          />
+        </div>
+      ) : (
+        <Text alignment='center' tone="critical">{file.name} is not supported.</Text>
+      )}
+    </div>
+  );
 
   const handleSubmit = useCallback(() => {
     if (title.trim() === "") {
@@ -174,9 +214,22 @@ export default function TemplateCreateForm() {
                   </Grid.Cell>
                 </Grid>
 
+                <br />
 
+                <Grid>
+                  <Grid.Cell columnSpan={{ xs: 10, sm: 10, md: 10, lg: 10, xl: 10 }}>
+                    {/* <Text variant="headingMd" as="h6">
+                      Use An Image for Better Perceptioin:
+                    </Text> */}
+                    <DropZone
+                      allowMultiple={false}
+                      onDrop={handleDropZoneDrop}>
+                      {uploadedFiles}
+                      {fileUpload}
+                    </DropZone>
+                  </Grid.Cell>
+                </Grid>
 
-<br/>
                 <ButtonGroup>
                   <Button url='/app/templates'>Cancel</Button>
                   <Button variant="primary" submit>Submit</Button>
